11ffca1ab63086ed6c74c938ad3afa037a9e673e
Michal Kaczkowski <sirmichal23@gazeta.pl>
2018-09-25 18:52:31 +0200

    dodanie opcji exit

--- src/main/java/com/sda/toDo/ToDoConsoleView.java
+++ src/main/java/com/sda/toDo/ToDoConsoleView.java
@@ -4,38 +4,40 @@ import java.util.Scanner;
 
 public class ToDoConsoleView {
     private Scanner scanner;
-    public ToDoConsoleView(Scanner scanner){
-        this.scanner=scanner;
-    }
-public Integer menu (){
-    System.out.println("Todo application");
-    System.out.println("1. Zaloguj");
-    System.out.println("2. Zarejestruj");
-    System.out.println("3. Dodaj zadanie");
-    System.out.println("4. Wyświetl zadania");
-    System.out.println("0. koniec");
-    int option = scanner.nextInt();
-    scanner.nextLine();
-    return option;
+
+    public ToDoConsoleView(Scanner scanner) {
+        this.scanner = scanner;
     }
 
-public String logInName (){
-    System.out.println("Podaj nick");
-    return scanner.nextLine();
-}
+    public Integer menu() {
+        System.out.println("Todo application");
+        System.out.println("1. Zaloguj");
+        System.out.println("2. Zarejestruj");
+        System.out.println("3. Dodaj zadanie");
+        System.out.println("4. Wyświetl zadania");
+        System.out.println("0. koniec");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        return option;
+    }
 
-public String loginPassword(){
-    System.out.println("Podaj haslo");
-    return scanner.nextLine();
-}
+    public String logInName() {
+        System.out.println("Podaj nick");
+        return scanner.nextLine();
+    }
 
-public String registerName(){
+    public String loginPassword() {
+        System.out.println("Podaj haslo");
+        return scanner.nextLine();
+    }
+
+    public String registerName() {
         return logInName();
-}
+    }
 
-public String registerPaswword (){
+    public String registerPaswword() {
         return loginPassword();
-}
+    }
 
     public String creatNewToDoName() {
         System.out.println("Podaj nazwe zadania");
@@ -47,13 +49,13 @@ public String registerPaswword (){
         return scanner.nextLine();
     }
 
-public void displayError(String message){
-    System.out.println();
-    System.out.println("Error");
-    System.out.println(message);
-    System.out.println("Error");
+    public void displayError(String message) {
+        System.out.println();
+        System.out.println("Error");
+        System.out.println(message);
+        System.out.println("Error");
 
-}
+    }
 
     public void displaySuccess(String message) {
         System.out.println();
@@ -61,5 +63,9 @@ public void displayError(String message){
         System.out.println(message);
         System.out.println("Success");
 
+    }
+
+    public void exit() {
+        System.out.println("Zapraszamy ponownie");
     }
 }
--- src/main/java/com/sda/toDo/views/toDoApplication.java
+++ src/main/java/com/sda/toDo/views/toDoApplication.java
@@ -23,80 +23,82 @@ public class toDoApplication {
     private ToDoUser currentUser;
 
 
-
-    public static void main(String[] args){
-    Scanner scanner = new Scanner (System.in);
-        ToDoRepository toDoRepository =new ToDoMemoryRepository();
-    ToDoUserRepository toDoUserRepository = new InMemomryToDoUserRepository();
-    ToDoService toDoService = new ToDoService(toDoRepository, toDoUserRepository);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ToDoRepository toDoRepository = new ToDoMemoryRepository();
+        ToDoUserRepository toDoUserRepository = new InMemomryToDoUserRepository();
+        ToDoService toDoService = new ToDoService(toDoRepository, toDoUserRepository);
         ToDoConsoleView toDoConsoleView = new ToDoConsoleView(scanner);
-        toDoApplication TodoApplication  = new toDoApplication (toDoService, toDoConsoleView,null);
-    TodoApplication.start();
+        toDoApplication TodoApplication = new toDoApplication(toDoService, toDoConsoleView, null);
+        TodoApplication.start();
 
 
-        System.out.println("Todo application");
+
     }
-public void start(){
 
-        do{
-     Integer menutoption = toDoConsoleView.menu();
-     switch (menutoption) {
-         case 1:
-             login();
-             break;
-         case 2:
-            registerUser();
-             break;
-         case 3:
-             addNewToDo();
-             break;
-         case 4:
-             break;
-             default:
-                 break;
+    public void start() {
+        Boolean flag = true;
+        do {
+            Integer menutoption = toDoConsoleView.menu();
+            switch (menutoption) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    registerUser();
+                    break;
+                case 3:
+                    addNewToDo();
+                    break;
+                case 4:
+                    break;
+                case 0:
+
+                default:
+                    toDoConsoleView.exit();
+                    flag = false;
+                    break;
 
 
-     }
-      }while(true);
-}
+            }
+        } while (flag);
+    }
 
     private void registerUser() {
-    String name=toDoConsoleView.registerName();
-    String password= toDoConsoleView.registerPaswword();
-    ToDoUser user = toDoService.register(name, password);
-     if(user==null){
-         toDoConsoleView.displayError("Nie można zarejestrować użytkownika. \n" + "uzytkownik o podanej nazwie juz istnieje");
-     }
+        String name = toDoConsoleView.registerName();
+        String password = toDoConsoleView.registerPaswword();
+        ToDoUser user = toDoService.register(name, password);
+        if (user == null) {
+            toDoConsoleView.displayError("Nie można zarejestrować użytkownika. \n" + "uzytkownik o podanej nazwie juz istnieje");
+        }
         toDoConsoleView.displaySuccess("Udalo sie zarejestrowac użytkownika" + name);
     }
 
     private void login() {
-        this.currentUser=null;
+        this.currentUser = null;
         String name = toDoConsoleView.logInName();
 
         String password = toDoConsoleView.loginPassword();
         try {
-            this.currentUser=toDoService.login(name,password);
-        } catch(ToDoUserDoesNotExists | InvalidPasswordException e){  //obsluga dwoch wyjatkow na raz
+            this.currentUser = toDoService.login(name, password);
+        } catch (ToDoUserDoesNotExists | InvalidPasswordException e) {  //obsluga dwoch wyjatkow na raz
             toDoConsoleView.displayError(e.getMessage());
         }
         if (this.currentUser != null) {
-            toDoConsoleView.displaySuccess("Uzytkownik o nicku \"" + name +"\" został zalogowany" );
+            toDoConsoleView.displaySuccess("Uzytkownik o nicku \"" + name + "\" został zalogowany");
         }
 
     }
 
 
-
-
-    private  void addNewToDo(){
-        if(currentUser==null){
+    private void addNewToDo() {
+        if (currentUser == null) {
             login();
 
         }
-        String todoName =toDoConsoleView.creatNewToDoName();
-        String todoDescription =toDoConsoleView.creatNewToDoDescritpion();
-         ToDo todo= new ToDo(todoName,this.currentUser);
+        String todoName = toDoConsoleView.creatNewToDoName();
+        String todoDescription = toDoConsoleView.creatNewToDoDescritpion();
+        ToDo todo = new ToDo(todoName, this.currentUser);
         todo.setDescription(todoDescription);
 
         toDoService.save(todo);
